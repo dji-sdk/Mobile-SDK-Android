95099a96fe5f30517a96095e11b462eb94f07275
Oliver Ou <oliver.ou@dji.com>
2017-09-15 17:06:52 +0800

    fix mediaPlaybackView issue.

--- Sample Code/app/src/main/java/com/dji/sdk/sample/demo/camera/MediaPlaybackView.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/demo/camera/MediaPlaybackView.java
@@ -369,12 +369,12 @@ public class MediaPlaybackView extends LinearLayout
 
     @Override
     public void onUpdate(MediaManager.VideoPlaybackState videoPlaybackState) {
-        post(new Runnable() {
-            @Override
-            public void run() {
-                ToastUtils.setResultToToast(" VideoPlaybackState on Result");
-            }
-        });
+        //post(new Runnable() {
+        //    @Override
+        //    public void run() {
+        //        ToastUtils.setResultToToast(" VideoPlaybackState on Result");
+        //    }
+        //});
         updateTextView(videoPlaybackState);
     }
 
