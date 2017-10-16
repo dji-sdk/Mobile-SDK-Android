dd15982809519fdd0624a40be341a5ec2ceb1c57
Oliver Ou <oliver.ou@dji.com>
2017-10-16 14:20:12 +0800

    Add dual gimbals control feature in sample code.

--- Sample Code/app/src/main/java/com/dji/sdk/sample/demo/gimbal/GimbalCapabilityView.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/demo/gimbal/GimbalCapabilityView.java
@@ -5,22 +5,42 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 import com.dji.sdk.sample.R;
+import com.dji.sdk.sample.internal.controller.DJISampleApplication;
+import com.dji.sdk.sample.internal.controller.MainActivity;
 import com.dji.sdk.sample.internal.utils.CallbackHandlers;
 import com.dji.sdk.sample.internal.utils.ToastUtils;
+import com.dji.sdk.sample.internal.utils.VideoFeedView;
 import com.dji.sdk.sample.internal.view.PresentableView;
+import dji.common.error.DJIError;
 import dji.common.gimbal.CapabilityKey;
 import dji.common.gimbal.GimbalMode;
 import dji.common.gimbal.Rotation;
 import dji.common.gimbal.RotationMode;
+import dji.common.product.Model;
 import dji.common.util.DJIParamCapability;
 import dji.common.util.DJIParamMinMaxCapability;
+import dji.keysdk.AirLinkKey;
+import dji.keysdk.KeyManager;
+import dji.keysdk.ProductKey;
+import dji.keysdk.callback.SetCallback;
 import dji.sdk.base.BaseProduct;
+import dji.sdk.camera.Camera;
+import dji.sdk.camera.VideoFeeder;
 import dji.sdk.gimbal.Gimbal;
+import dji.sdk.products.Aircraft;
 import dji.sdk.sdkmanager.DJISDKManager;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * The class for rotating the gimbal, also give an example of the product with multiple gimbals.
+ */
 public class GimbalCapabilityView extends LinearLayout implements View.OnClickListener, PresentableView {
 
     private Gimbal gimbal = null;
@@ -30,10 +50,31 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
     private Button yawMaxBtn;
     private Button rollMinBtn;
     private Button rollMaxBtn;
+    private Button mBtnSet;
+    private Button leftAndRightBtn;
+    private Button leftAndFpvBtn;
+    private Button rightAndFpvBtn;
+    private Spinner mSpinnerSet;
+    private View primaryCoverView;
+    private View fpvCoverView;
+    private TextView cameraListTitle;
+    private TextView primaryVideoFeedTitle;
+    private TextView fpvVideoFeedTitle;
+    private VideoFeedView primaryVideoFeed;
+    private VideoFeedView fpvVideoFeed;
+
+    private String cameraListStr;
+    private int currentGimbalId = 0;
+    private AirLinkKey lbBandwidthKey;
+    private AirLinkKey mainCameraBandwidthKey;
+    private SetCallback setBandwidthCallback;
+    private VideoFeeder.PhysicalSourceListener sourceListener;
 
     public GimbalCapabilityView(Context context) {
         super(context);
         initUI(context);
+        initAllKeys();
+        initCallbacks();
     }
 
     @NonNull
@@ -45,6 +86,7 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+        DJISampleApplication.getEventBus().post(new MainActivity.RequestStartFullScreenEvent());
         setupButtonsState();
         enablePitchExtensionIfPossible();
         if (getGimbalInstance() != null) {
@@ -52,27 +94,54 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
         } else {
             ToastUtils.setResultToToast("Product disconnected");
         }
+        setUpListeners();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        DJISampleApplication.getEventBus().post(new MainActivity.RequestEndFullScreenEvent());
+        currentGimbalId = 0;
+        tearDownListeners();
+        super.onDetachedFromWindow();
     }
 
     private void setupButtonsState() {
+
+        int size = getGimbalSize();
+        if (size <1) {
+            mSpinnerSet.setEnabled(false);
+        } else {
+            mSpinnerSet.setAdapter(getArrayAdapter(size));
+        }
         pitchMinBtn.setEnabled(isFeatureSupported(CapabilityKey.ADJUST_PITCH));
         pitchMaxBtn.setEnabled(isFeatureSupported(CapabilityKey.ADJUST_PITCH));
         yawMinBtn.setEnabled(isFeatureSupported(CapabilityKey.ADJUST_YAW));
         yawMaxBtn.setEnabled(isFeatureSupported(CapabilityKey.ADJUST_YAW));
         rollMinBtn.setEnabled(isFeatureSupported(CapabilityKey.ADJUST_ROLL));
         rollMaxBtn.setEnabled(isFeatureSupported(CapabilityKey.ADJUST_ROLL));
+        leftAndFpvBtn.setEnabled(false);
+        rightAndFpvBtn.setEnabled(false);
+        leftAndRightBtn.setEnabled(false);
     }
 
     private Gimbal getGimbalInstance() {
         if (gimbal == null) {
-            if (DJISDKManager.getInstance() != null) {
-                BaseProduct product = DJISDKManager.getInstance().getProduct();
-                if (product != null) {
+            initGimbal();
+        }
+        return gimbal;
+    }
+
+    private void initGimbal() {
+        if (DJISDKManager.getInstance() != null) {
+            BaseProduct product = DJISDKManager.getInstance().getProduct();
+            if (product != null) {
+                if (product instanceof Aircraft) {
+                    gimbal = ((Aircraft) product).getGimbals().get(currentGimbalId);
+                } else {
                     gimbal = product.getGimbal();
                 }
             }
         }
-        return gimbal;
     }
 
     /*
@@ -131,12 +200,14 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
 
     private void initUI(Context context) {
         setClickable(true);
-        setOrientation(VERTICAL);
+        setOrientation(HORIZONTAL);
 
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Service.LAYOUT_INFLATER_SERVICE);
 
         layoutInflater.inflate(R.layout.view_gimbal_capability, this, true);
 
+        mSpinnerSet = (Spinner) findViewById(R.id.spinner_set);
+        mBtnSet = (Button) findViewById(R.id.btn_set);
         pitchMinBtn = (Button) findViewById(R.id.btn_pitchMin);
         pitchMaxBtn = (Button) findViewById(R.id.btn_pitchMax);
         yawMinBtn = (Button) findViewById(R.id.btn_yawMin);
@@ -144,7 +215,20 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
         rollMinBtn = (Button) findViewById(R.id.btn_rollMin);
         rollMaxBtn = (Button) findViewById(R.id.btn_rollMax);
         Button resetBtn = (Button) findViewById(R.id.btn_reset);
+        leftAndFpvBtn = (Button) findViewById(R.id.video_feeder_left_fpv);
+        rightAndFpvBtn = (Button) findViewById(R.id.video_feeder_right_fpv);
+        leftAndRightBtn = (Button) findViewById(R.id.video_feeder_left_right);
+        cameraListTitle = (TextView) findViewById(R.id.camera_index_title);
+        primaryVideoFeedTitle = (TextView) findViewById(R.id.primary_video_feed_title);
+        fpvVideoFeedTitle = (TextView) findViewById(R.id.fpv_video_feed_title);
+        primaryVideoFeed = (VideoFeedView) findViewById(R.id.primary_video_feed);
+        fpvVideoFeed = (VideoFeedView) findViewById(R.id.fpv_video_feed);
+        primaryCoverView = findViewById(R.id.primary_cover_view);
+        primaryVideoFeed.setCoverView(primaryCoverView);
+        fpvCoverView = findViewById(R.id.fpv_cover_view);
+        fpvVideoFeed.setCoverView(fpvCoverView);
 
+        mBtnSet.setOnClickListener(this);
         pitchMinBtn.setOnClickListener(this);
         pitchMaxBtn.setOnClickListener(this);
         yawMinBtn.setOnClickListener(this);
@@ -152,6 +236,34 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
         rollMinBtn.setOnClickListener(this);
         rollMaxBtn.setOnClickListener(this);
         resetBtn.setOnClickListener(this);
+        leftAndFpvBtn.setOnClickListener(this);
+        rightAndFpvBtn.setOnClickListener(this);
+        leftAndRightBtn.setOnClickListener(this);
+    }
+
+    private void initAllKeys() {
+        lbBandwidthKey = AirLinkKey.createLightbridgeLinkKey(AirLinkKey.BANDWIDTH_ALLOCATION_FOR_LB_VIDEO_INPUT_PORT);
+        mainCameraBandwidthKey = AirLinkKey.createLightbridgeLinkKey(AirLinkKey.BANDWIDTH_ALLOCATION_FOR_LEFT_CAMERA);
+    }
+
+    private void initCallbacks() {
+        setBandwidthCallback = new SetCallback() {
+            @Override
+            public void onSuccess() {
+                ToastUtils.setResultToToast("Set key value successfully");
+                if (fpvVideoFeed != null) {
+                    fpvVideoFeed.changeSourceResetKeyFrame();
+                }
+                if (primaryVideoFeed != null) {
+                    primaryVideoFeed.changeSourceResetKeyFrame();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull DJIError error) {
+                ToastUtils.setResultToToast("Failed to set: " + error.getDescription());
+            }
+        };
     }
 
     private void rotateGimbalToMin(Button button) {
@@ -200,6 +312,21 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
         sendRotateGimbalCommand(builder.build());
     }
 
+    private void onClickLeftAndFpvBtn() {
+        KeyManager.getInstance().setValue(lbBandwidthKey, 0.8f, null);
+        KeyManager.getInstance().setValue(mainCameraBandwidthKey, 1.0f, setBandwidthCallback);
+    }
+
+    private void onClickRightAndFpvBtn() {
+        KeyManager.getInstance().setValue(lbBandwidthKey, 0.8f, null);
+        KeyManager.getInstance().setValue(mainCameraBandwidthKey, 0.0f, setBandwidthCallback);
+    }
+
+    private void onClickLeftAndRightBtn() {
+        KeyManager.getInstance().setValue(lbBandwidthKey, 1.0f, null);
+        KeyManager.getInstance().setValue(mainCameraBandwidthKey, 0.5f, setBandwidthCallback);
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -228,12 +355,180 @@ public class GimbalCapabilityView extends LinearLayout implements View.OnClickLi
                 break;
             }
             case R.id.btn_reset: {
-                getGimbalInstance().reset(null);
+                Gimbal gimbal = getGimbalInstance();
+                if (gimbal != null) {
+                    gimbal.reset(null);
+                } else {
+                    ToastUtils.setResultToToast("The gimbal is disconnected.");
+                }
+                break;
+            }
+            case R.id.btn_set: {
+                int position = mSpinnerSet.getSelectedItemPosition();
+                if (position >= 0 && position < getGimbalSize()) {
+                    currentGimbalId = mSpinnerSet.getSelectedItemPosition();
+                    initGimbal();
+                    ToastUtils.setResultToToast("Selected gimbal ID:" + currentGimbalId);
+                } else {
+                    ToastUtils.setResultToToast("The gimbal ID is illegal.");
+                }
+                break;
+            }
+            case R.id.video_feeder_left_fpv: {
+                onClickLeftAndFpvBtn();
+                break;
+            }
+            case R.id.video_feeder_right_fpv: {
+                onClickRightAndFpvBtn();
+                break;
+            }
+            case R.id.video_feeder_left_right: {
+                onClickLeftAndRightBtn();
                 break;
             }
             default:
                 break;
         }
+    }
+
+    private ArrayAdapter getArrayAdapter(int size) {
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            array.add(i);
+        }
+        return new ArrayAdapter(this.getContext(), R.layout.simple_list_item, array);
+    }
+
+    private int getGimbalSize() {
+        int size = 0;
+        if (DJISDKManager.getInstance() != null) {
+            BaseProduct product = DJISDKManager.getInstance().getProduct();
+            if (product != null) {
+                if (product instanceof Aircraft) {
+                    size = ((Aircraft) product).getGimbals().size();
+                } else {
+                    if (product.getGimbal() != null) {
+                        size = 1;
+                    }
+                }
+            }
+        }
+        return size;
+    }
+
+    private void setUpListeners() {
+        sourceListener = new VideoFeeder.PhysicalSourceListener() {
+            @Override
+            public void onChange(VideoFeeder.VideoFeed videoFeed, VideoFeeder.PhysicalSource newPhysicalSource) {
+                if (videoFeed == VideoFeeder.getInstance().getPrimaryVideoFeed()) {
+                    String newText = "Primary Source: " + newPhysicalSource.toString();
+                    ToastUtils.setResultToText(primaryVideoFeedTitle,newText);
+                }
+                if (videoFeed == VideoFeeder.getInstance().getSecondaryVideoFeed()) {
+                    ToastUtils.setResultToText(fpvVideoFeedTitle,"Secondary Source: " + newPhysicalSource.toString());
+                }
+            }
+        };
+
+        setVideoFeederListeners(true);
+    }
+
+    private void tearDownListeners() {
+        setVideoFeederListeners(false);
+    }
+
+    private void setVideoFeederListeners(boolean isOpen) {
+        if (VideoFeeder.getInstance() == null) return;
+
+        final BaseProduct product = DJISDKManager.getInstance().getProduct();
+        updateM210Buttons();
+        if (product != null) {
+
+            if (isOpen) {
+                primaryVideoFeed.registerLiveVideo(VideoFeeder.getInstance().getPrimaryVideoFeed(), true);
+                String newText = "Primary Source: " + VideoFeeder.getInstance().getPrimaryVideoFeed().getVideoSource().name();
+                ToastUtils.setResultToText(primaryVideoFeedTitle,newText);
+                if (isMultiStreamPlatform()) {
+                    fpvVideoFeed.registerLiveVideo(VideoFeeder.getInstance().getSecondaryVideoFeed(), false);
+                    String newTextFpv = "Secondary Source: " + VideoFeeder.getInstance().getSecondaryVideoFeed().getVideoSource().name();
+                    ToastUtils.setResultToText(fpvVideoFeedTitle,newTextFpv);
+                }
+                VideoFeeder.getInstance().addPhysicalSourceListener(sourceListener);
+            } else {
+                VideoFeeder.getInstance().removePhysicalSourceListener(sourceListener);
+                VideoFeeder.getInstance().getPrimaryVideoFeed().setCallback(null);
+                if (isMultiStreamPlatform()) {
+                    VideoFeeder.getInstance().getSecondaryVideoFeed().setCallback(null);
+                }
+            }
+        }
+    }
+
+    private void updateM210Buttons() {
+        if (isM210TwoCameraConnected()) {
+
+            GimbalCapabilityView.this.post(new Runnable() {
+                @Override
+                public void run() {
+                    leftAndFpvBtn.setEnabled(true);
+                    rightAndFpvBtn.setEnabled(true);
+                    leftAndRightBtn.setEnabled(true);
+                }
+            });
+        }
+    }
+
+    private boolean isM210TwoCameraConnected() {
+
+        Object model = null;
+        if (KeyManager.getInstance() != null) {
+            model = KeyManager.getInstance().getValue(ProductKey.create(ProductKey.MODEL_NAME));
+        }
+        if (model != null) {
+            BaseProduct product = DJISDKManager.getInstance().getProduct();
+            if (product != null && (product instanceof Aircraft)) {
+                List<Camera> cameraList = ((Aircraft) product).getCameras();
+                if (cameraList != null) {
+                    if (cameraListStr == null) {
+                        cameraListStr = new String();
+                    }
+                    cameraListStr = "";
+                    for (int i = 0; i < cameraList.size(); i++) {
+                        Camera camera = cameraList.get(i);
+                        cameraListStr += "Camera "
+                            + i
+                            + " is "
+                            + camera.getDisplayName()
+                            + " is connected "
+                            + camera.isConnected()
+                            + " camera component index is "
+                            + +camera.getIndex()
+                            + "\n";
+                    }
+                }
+                ToastUtils.setResultToText(cameraListTitle, cameraListStr);
+                if ((model == Model.MATRICE_210 || model == Model.MATRICE_210_RTK)) {
+                    return (cameraList != null
+                        && cameraList.size() == 2
+                        && cameraList.get(0).isConnected()
+                        && cameraList.get(1).isConnected());
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isMultiStreamPlatform() {
+        Model model = DJISDKManager.getInstance().getProduct().getModel();
+        return model != null && (model == Model.INSPIRE_2
+            || model == Model.MATRICE_200
+            || model == Model.MATRICE_210
+            || model == Model.MATRICE_210_RTK
+            || model == Model.MATRICE_600
+            || model == Model.MATRICE_600_PRO
+            || model == Model.A3
+            || model == Model.N3);
     }
 
     @Override
