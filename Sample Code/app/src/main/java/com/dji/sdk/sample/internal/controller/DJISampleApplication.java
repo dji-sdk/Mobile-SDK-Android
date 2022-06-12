21b096530117ddcefeeb6972747a7fdea9979e4f
Hannes Achleitner <hannes.software@gmx.at>
2022-06-12 06:30:52 +0200

    Bump libraries

--- Sample Code/app/src/main/java/com/dji/sdk/sample/internal/controller/DJISampleApplication.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/internal/controller/DJISampleApplication.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import com.squareup.otto.Bus;
 import com.squareup.otto.ThreadEnforcer;
 
-import androidx.multidex.MultiDex;
 import dji.sdk.base.BaseProduct;
 import dji.sdk.products.Aircraft;
 import dji.sdk.products.HandHeld;
@@ -73,7 +72,6 @@ public class DJISampleApplication extends Application {
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
-        MultiDex.install(this);
         com.secneo.sdk.Helper.install(this);
         app = this;
     }
